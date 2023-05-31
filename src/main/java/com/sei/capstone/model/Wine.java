9a74773e5d182477c753386049182a2796ace5f0
Kelsey Halley <kelseyghalley@gmail.com>
2023-05-31 11:48:14 -0700

    added wine class

---
+++ src/main/java/com/sei/capstone/model/Wine.java
@@ -0,0 +1,14 @@
+package com.sei.capstone.model;
+
+public class Wine {
+    private String name;
+    private String vintage;
+    private String varietal;
+    private String vitner;
+    private String tastingNotes;
+    private String glassWare;
+    private String aerationTime;
+    private Integer servingTemperature;
+    private String pairing;
+
+}
