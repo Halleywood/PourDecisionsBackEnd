239236298d459659f4ec62e1ac82742150e6e655
Kelsey Halley <kelseyghalley@gmail.com>
2023-05-31 11:43:07 -0700

    set up file tree, creating models

---
+++ src/main/java/com/sei/capstone/model/User.java
@@ -0,0 +1,8 @@
+package com.sei.capstone.model;
+
+public class User {
+    private Long id;
+    private String email;
+    private String password;
+
+}
---
+++ src/main/java/com/sei/capstone/model/UserProfile.java
@@ -0,0 +1,8 @@
+package com.sei.capstone.model;
+
+public class UserProfile {
+    private Long profileId;
+    private String userName;
+    private String tagline;
+
+}
