b6145d5741080dc3a6044f7a22675edf93cda8e2
Kelsey Halley <kelseyghalley@gmail.com>
2023-06-01 07:00:27 -0700

    added login models

---
+++ src/main/java/com/sei/capstone/model/login/LoginResponse.java
@@ -0,0 +1,19 @@
+package com.sei.capstone.model.login;
+
+public class LoginResponse {
+
+        private String message;
+
+        public LoginResponse(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+}
